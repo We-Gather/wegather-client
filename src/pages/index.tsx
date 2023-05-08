8e05b48d60f5cc07cd5a2bcf1980d38f29f340b4
주재현 <jake010417@gmail.com>
2023-05-08 22:23:50 +0900

    Sign up page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,12 @@
-import Container from "@/components/Container";
 
 export default function Home() {
   return (
     <>
-    <Container>
       <section className=" flex-col md:flex-row flex items-center md:justify-between mb-16 md:mb-12">
           <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
             test.
           </h1>
         </section>
-    </Container>
     </>
   )
 }
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,9 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Link from 'next/link';
+import HorizonLine from '@/components/HorizonLine';
+import LoginButton from '@/components/button/LoginButton';
+
+const SignUpPageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 70%;
+  width: 40%;
+  justify-content: center;
+  align-items: center;
+  place-content: center;
+  border-radius: 0.5rem;
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+`;
+const Form = styled.form`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  place-content: center;
+  display: flex;
+  flex-direction: column;
+`;
+const StyledP = styled.p`
+  margin-bottom: 3%;
+`;
+const StyledLoginInput = styled.input`
+  border: 1px solid #e5e7eb;
+  border-radius: 10px;
+  width: 80%;
+  height: 100%;
+  margin-bottom: 3%;
+  transition: background-color 0.2s ease-in-out;
+  &:focus {
+  background-color: #ffffff;
+  }
+`;
+const RowWrapper= styled.div`
+  width: 80%;
+  height: 10%;
+  display: flex;
+  margin: 1.5%;
+  font-size: 0.75rem;
+  flex-direction: row;
+  justify-content: space-between;
+	align-items: center;
+`;
+
+type signUpInfoType = {
+  email: string;
+  password: string;
+  name: string;
+  school: string;
+};
+
 
 export default function SignUpPage() {
+  const [signUpInfo, setSignUpInfo] = useState<signUpInfoType>({
+    email: "",
+    password: "",
+    name: "",
+    school: "",
+  });
+  const [passwordCheck, setPasswordCheck] = useState('');
+
   return (
-    <div>
-      Sign Up Page
-    </div>
+    <SignUpPageDiv>
+      <Form>
+        <h1>
+          위게더 회원가입
+        </h1>
+        <StyledP>위게더의 회원이 되어 다양한 서비스를 경험해 보세요</StyledP>
+        <HorizonLine/>
+        <RowWrapper>
+          <StyledP>test</StyledP>
+          <StyledLoginInput type="email" 
+            value={signUpInfo.email} 
+            placeholder="*이메일" 
+            onChange={(e) => setSignUpInfo({
+              ...signUpInfo,
+              email: e.target.value,
+            })}
+            required 
+          />
+        </RowWrapper>
+        <RowWrapper>
+        <StyledP>test</StyledP>
+
+          <StyledLoginInput type="password" 
+            value={signUpInfo.password} 
+            placeholder="*비밀번호" 
+            onChange={(e) => setSignUpInfo({
+              ...signUpInfo,
+              password: e.target.value,
+            })}
+            required 
+          />
+        </RowWrapper>
+        <RowWrapper>
+        <StyledP>test</StyledP>
+
+          <StyledLoginInput type="passwordCheck" 
+            value={passwordCheck} 
+            placeholder="*비밀번호 확인" 
+            onChange={(e) => setPasswordCheck(e.target.value)} 
+            required 
+          />
+        </RowWrapper>
+        <RowWrapper>
+        <StyledP>test</StyledP>
+
+          <StyledLoginInput type="name" 
+            value={signUpInfo.password} 
+            placeholder="*이름" 
+            onChange={(e) => setSignUpInfo({
+              ...signUpInfo,
+              name: e.target.value,
+            })}
+            required 
+          />
+        </RowWrapper>
+        <RowWrapper>
+        <StyledP>test</StyledP>
+
+          <StyledLoginInput type="school" 
+            value={signUpInfo.password} 
+            placeholder="*학교" 
+            onChange={(e) => setSignUpInfo({
+              ...signUpInfo,
+              school: e.target.value,
+            })}
+            required 
+          />
+        </RowWrapper>
+        <LoginButton text="회원가입" type="submit"/>
+      </Form>
+    </SignUpPageDiv>
   );
 };
