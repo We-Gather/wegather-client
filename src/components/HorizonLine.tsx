99ef98170c9839453c410a8ffb6c481baa8c41c2
0816i <0816i@naver.com>
2023-07-08 14:32:32 +0900

    build: test

--- src/components/HorizonLine.tsx
+++ src/components/HorizonLine.tsx
@@ -1,31 +1,26 @@
-import React from "react"
+import React from "react";
 import styled from "styled-components";
 
 const HorizonLineDiv = styled.div`
-	width: 22.5rem;
-	margin-bottom: 0.75rem;
-	text-align: center;
-	line-height: 0.1rem;
-	border-bottom: 0.1rem solid #aaa;
+  width: 22.5rem;
+  margin-bottom: 0.75rem;
+  text-align: center;
+  line-height: 0.1rem;
+  border-bottom: 0.1rem solid #aaa;
 `;
 
 const HorizonLineSpan = styled.span`
-	background-color: #F2F2F2;
-	font-size: 0.75rem;
-	padding: 0.5rem;
+  background-color: #f2f2f2;
+  font-size: 0.75rem;
+  padding: 0.5rem;
 `;
 
+export default function HorizonLine({ text }: any) {
+  if (!text) return <HorizonLineDiv />;
 
-export default function HorizonLine({text} : any) {
-	if (!text) return (
-		<HorizonLineDiv/>
-	); 
-
-	return (
-		<HorizonLineDiv>
-			<HorizonLineSpan>
-				{text}
-			</HorizonLineSpan>
-		</HorizonLineDiv>
-	);
+  return (
+    <HorizonLineDiv>
+      <HorizonLineSpan>{text}</HorizonLineSpan>
+    </HorizonLineDiv>
+  );
 }
