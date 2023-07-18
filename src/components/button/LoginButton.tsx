a90b5fdaa5b22cb8618a2b17282b7891c0055d80
joojae02 <jake010417@gmail.com>
2023-07-18 20:17:33 +0900

    feat: 로그인 페이지 버튼 컴포넌트 구현

---
+++ src/components/button/LoginButton.tsx
@@ -0,0 +1,32 @@
+import { ButtonHTMLAttributes } from "react";
+import styled from "styled-components";
+
+const SubmitButton = styled.button`
+  background-color: #36BF7F;
+  color: #ffffff;
+  width: 22.5rem;
+  height: 3.75rem;
+  flex-shrink: 0;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  border: #36BF7F;
+
+  &:hover {
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	text: string;
+	type?: "button" | "submit" | "reset";
+};
+
+export default function LoginButton({text, type = "submit", ...rest}: ButtonProps) {
+	return (
+		<SubmitButton type={type} {...rest} >
+			{text}
+		</SubmitButton>
+
+	);
+}
