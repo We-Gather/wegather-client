5b723344425213fd5dfc871a22d17f4d27eaf75f
joojae02 <jake010417@gmail.com>
2023-07-18 20:18:39 +0900

    feat: header, footer 포함 layout  컴포넌트 구현

---
+++ src/components/layout/HeaderFooterLayout.tsx
@@ -0,0 +1,29 @@
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+import React from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const StyledMain = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  place-content: center;
+  height: calc(100% - 13.5rem);
+`;
+
+export default function HeaderFooterLayout({ children }: any) {
+  return (
+    <StyledContainer>
+      <Header />
+      <StyledMain>{children}</StyledMain>
+      <Footer />
+    </StyledContainer>
+  );
+}
