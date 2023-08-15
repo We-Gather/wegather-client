9e394bc2081e643c4fed20b5930e0e7356a6d7b4
joojae02 <jake010417@gmail.com>
2023-08-15 19:35:58 +0900

    feat: 동아리 메인 페이지 구현, 정보, 모집공고리스트, 추가 설명란

---
+++ src/pages/clubMain/ClubInfoRecruitCardList.tsx
@@ -0,0 +1,93 @@
+import ClubRecruitCard from '@/components/ClubRecruitCard';
+import SortDropDown from '@/components/dropdown/SortDropDown';
+import { clubRecruitCard } from '@/types/clubRecruitCard';
+import { styled } from 'styled-components';
+import { SearchAlt2 } from '@styled-icons/boxicons-regular/SearchAlt2';
+const MainContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+
+const SearchSortContainer = styled.div`
+	height: 6.5rem;
+	width: 62.5rem;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+const InputWrapper = styled.div`
+	position: relative;
+	display: block;
+`;
+const RecruitSearchInput = styled.input`
+	width: 23.75rem;
+	height: 2.5rem;
+	flex-shrink: 0;
+	border-radius: 1.875rem;
+	border: 1px solid #c9c9c9;
+	background: #fff;
+`;
+const SearchButton = styled.button`
+	display: inline-block;
+	position: absolute;
+	height: 100%;
+	right: 1rem;
+	background-color: transparent;
+	border: none;
+	color: #fff;
+`;
+const SearchIcon = styled(SearchAlt2)`
+	width: 1.5rem;
+	height: 1.5rem;
+	color: #585858;
+`;
+const RecruitCardContainer = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const RecruitCardWrapper = styled.div`
+	display: flex;
+	align-items: center;
+	margin-bottom: 1.5rem;
+`;
+
+const mockRecruitCard: clubRecruitCard = {
+	title: 'UX/UI 디자이너 모집',
+	description:
+		'위게더 22기 디자이너를 모집합니다! 웹사이트 메인페이지 디자인을 담당하게 될 예정입니다. 성실하게 참여하실 분 이라면 누구든 환영합니다. (최대 2줄)',
+	startDate: '2023-06-29',
+	endDate: '2023-06-29',
+	applicantCount: 10,
+};
+
+const mockCardList: Array<clubRecruitCard> = [mockRecruitCard, mockRecruitCard, mockRecruitCard, mockRecruitCard];
+const dropDownList = ['최신순', '즐겨찾기순', '마감일순'];
+
+export default function ClubInfoRecruitCardList() {
+	return (
+		<MainContainer>
+			<SearchSortContainer>
+				<InputWrapper>
+					<RecruitSearchInput />
+					<SearchButton>
+						<SearchIcon/>
+					</SearchButton>
+				</InputWrapper>
+				<SortDropDown list={dropDownList} />
+			</SearchSortContainer>
+			<RecruitCardContainer>
+				{mockCardList.map((recruitInfo, index) => (
+					<RecruitCardWrapper key={index}>
+						<ClubRecruitCard recruitInfo={recruitInfo} />
+					</RecruitCardWrapper>
+				))}
+			</RecruitCardContainer>
+		</MainContainer>
+	);
+}
--- src/pages/clubMain/index.tsx
+++ src/pages/clubMain/index.tsx
@@ -5,29 +5,125 @@ import { useState } from 'react';
 
 const ClubMainContainer = styled.div`
 	display: flex;
-  flex-direction: column;
-  justify-content: center;
-  place-content: center;
-  align-items: center;
+	flex-direction: column;
+	justify-content: center;
+	place-content: center;
+	align-items: center;
 `;
 
+const TabMenu = styled.ul`
+	width: 65rem;
+	border-radius: 0.625rem 0.625rem 0rem 0rem;
+	/* border: 1px solid #C9C9C9; */
+	background: #F4F6F8;
+	color: rgb(232, 234, 237);
+	font-weight: bold;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: flex-end;
+	list-style: none;
+	margin-top: 10px;
+
+	.submenu {
+		display: flex;
+		width: calc(100% / 4);
+		padding: 10px;
+		color: #000;
+		align-items: center;
+		justify-content: center;
+		transition: 0.5s;
+		width: 15.625rem;
+		height: 3.75rem;
+		border-radius: 0.625rem 0.625rem 0rem 0rem;
+		border: 1px solid #c9c9c9;
+		background: #e5e5e5;
+	}
+
+	.focused {
+		height: 4.375rem;
+		border-top: 1px solid #585858;
+		border-right: 1px solid #585858;
+		border-left: 1px solid #585858;
+		border-bottom: 1px solid #fff;
+		background: #fff;
+	}
+
+	& div.desc {
+		text-align: center;
+	}
+`;
+
+const TapMenuWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-top: 2rem;
+`;
+
+const Desc = styled.div`
+	width: 71.875rem;
+	min-height: 28.25rem;
+	text-align: center;
+	flex-shrink: 0;
+	border-radius: 0.625rem;
+	background: #fff;
+`;
+const PosterWrapper = styled.div`
+	width: 71.875rem;
+	height: 40rem;
+	flex-shrink: 0;
+	margin-top: 4rem;
+	border-radius: 0.625rem;
+	border: 1px solid #d9d9d9;
+	background: #fff;
+`;
+
+const menuArr = [
+	{ id: 1, name: '모집공고'},
+	{ id: 2, name: '동아리 정보'},
+	{ id: 3, name: '동아리 활동'},
+	{ id: 4, name: '회계'},
+];
+
 const mockProfile: clubProfile = {
-  name: '웹사이트 제작 동아리 Wegather',
-  image: '/public/cat.jpg',
-  description: '동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리\
+	name: '웹사이트 제작 동아리 Wegather',
+	image: '/public/cat.jpg',
+	description: '동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리 설명 동아리\
 								동아리 설명 동아리 설명 동아리 설명 동아리 설명 (최대 2줄)',
 	url: 'test',
-	tag: ['test','test','test','test','test','test','test']
+	tag: ['text', 'text', 'text', 'text', 'text'],
 };
+
 export default function ClubMain() {
 	const [currentTab, clickTab] = useState(0);
-  const selectMenuHandler = (index: number) => {
-    clickTab(index);
-  };
+	const selectMenuHandler = (index: number) => {
+		clickTab(index);
+	};
 
 	return (
 		<ClubMainContainer>
-			<ClubProfile profile={mockProfile}/>
+			<ClubProfile profile={mockProfile} />
+			<TapMenuWrapper>
+				<TabMenu>
+					{menuArr.map((tap, index) => {
+						return (
+							<li
+								key={index}
+								className={currentTab === index ? 'submenu focused' : 'submenu'}
+								onClick={() => selectMenuHandler(index)}
+							>
+								{tap.name}
+							</li>
+						);
+					})}
+				</TabMenu>
+				<Desc>
+					<ClubInfoRecruitCardList />
+					{/* <p>{menuArr[currentTab].content}</p> */}
+				</Desc>
+			</TapMenuWrapper>
+			<PosterWrapper></PosterWrapper>
 		</ClubMainContainer>
 	);
 }
