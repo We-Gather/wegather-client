6441bf4cc95e2f41e5d76d085021bcf30bbe2a1f
joojae02 <jake010417@gmail.com>
2023-08-29 20:48:33 +0900

    feat: club 생성 type 정의

---
+++ src/types/clubCreationInfo.ts
@@ -0,0 +1,12 @@
+
+export interface clubCreationInfo {
+	name: string;
+	type: string;
+	schoolId: number;
+	logo: string;
+	introduction: string;
+	explanation: string;
+	catagory: string;
+	tag: string;
+	poster: string;
+};
