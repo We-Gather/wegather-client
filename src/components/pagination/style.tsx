3dca7d2142ddc4069d2bca69748e08e14335a8fb
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-09-12 14:02:07 +0900

    feat: 페이징 컴포넌트 개발 WGT-139

---
+++ src/components/pagination/style.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const PaginationWrapper = styled.div`
+  display: flex;
+  cursor: pointer;
+  font-weight: 800;
+  align-items: center;
+`;
+
+export const PageButton = styled.div<{ selected?: boolean }>`
+  width: 1.625rem;
+  height: 1.625rem;
+  flex-shrink: 0;
+  border-radius: 50px;
+  color: ${(props) => (props.selected ? 'white' : 'black')};
+  background-color: ${(props) => (props.selected ? '#36BF7F' : '')};
+  margin: 0 0.7rem 0 0.7rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PrevNextButton = styled.button`
+  color: #585858;
+  text-align: center;
+  font-family: NanumSquareOTF_ac;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 0.0625rem;
+  margin: 0 0.7rem 0 0.7rem;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  &:disabled {
+    color: #c9c9c9;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  cursor: pointer;
+`;
