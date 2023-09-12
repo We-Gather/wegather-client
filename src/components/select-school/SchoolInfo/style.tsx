48bca59c85856e9e28ddf810391f0602383b6d49
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-09-12 14:28:26 +0900

    feat: 학교 선택 화면 개발 (WGT-139)

---
+++ src/components/select-school/SchoolInfo/style.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const ContainerDiv = styled.div`
+  width: 8.75rem;
+  height: 8.125rem;
+  flex-shrink: 0;
+  border-radius: 1.5625rem;
+  border: 1px solid #9c9c9c;
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem 0;
+  cursor: pointer;
+  position: relative;
+`;
+
+export const SchoolLogoDiv = styled.div<{ url: string }>`
+  width: 4.375rem;
+  height: 4.375rem;
+  flex-shrink: 0;
+  background-image: url(${(props) => props.url});
+  background-repeat: no-repeat;
+  background-size: 4.375rem 4.375rem;
+`;
+
+export const Border = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #d9d9d9;
+`;
+
+export const SchoolNameDiv = styled.div`
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 150%;
+  letter-spacing: 0.0625rem;
+`;
+
+export const VerticalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const MarkWrapper = styled.div`
+  position: absolute;
+  right: 0.5rem;
+  height: 1.25rem;
+  width: 1.25rem;
+  flex-shrink: 0;
+`;
---
+++ src/pages/select-school/index.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import {
+  ContainerDiv,
+  TitleDiv,
+  TitleDescriptionDiv,
+  SchoolListHeaderDiv,
+  SearchInput,
+  DirectButton,
+  SchoolListContentDiv,
+  PaginationWrapper,
+  SchoolListToolbarDiv,
+  SortButton,
+  BorderLine,
+  SortByWrapper,
+  SearchIcon,
+  ButtonWrapper,
+  SchoolListContent,
+  SchoolCardWrapper,
+} from '@/styles/select-school/style';
+import Pagination from '@/components/pagination';
+import SchoolInfo from '@/components/select-school/SchoolInfo';
+
+/**
+ * 학교 선택 페이지
+ */
+export default function SelectSchool(): JSX.Element {
+  const [page, setPage] = useState(1);
+  const [sortByList] = useState([
+    { label: '오름차순 (ㄱ ~ ㅎ)', value: '' },
+    { label: '내림차순 (ㅎ ~ ㄱ)', value: '' },
+    { label: '인기순', value: '' },
+  ]);
+  const [curSortBy, setCurSortBy] = useState(sortByList[0]);
+  // 로그인, 인증 여부 (임시)
+  const isLogin = true;
+  const isAuthenticated = true;
+
+  return (
+    <ContainerDiv>
+      {/* 상단 타이틀 */}
+      <TitleDiv>
+        <div>학교 선택</div>
+        <TitleDescriptionDiv>
+          카드 클릭 시 해당 학교의 <span>동아리 리스트</span>로 이동합니다.
+        </TitleDescriptionDiv>
+      </TitleDiv>
+
+      <div>
+        {/* 학교 선택 컴포넌트 상단 검색창 및 버튼 */}
+        <SchoolListHeaderDiv>
+          {isLogin && <h3>다른 학교 둘러보기</h3>}
+          <SearchInput>
+            <input placeholder="검색"></input>
+            <SearchIcon />
+          </SearchInput>
+          {isLogin && (
+            <ButtonWrapper>
+              {!isAuthenticated && (
+                <DirectButton>
+                  <div>
+                    <b>학교 인증</b> 바로가기
+                  </div>
+                </DirectButton>
+              )}
+              <DirectButton colored="true">
+                <div>
+                  <b>내 동아리</b> 바로가기
+                </div>
+              </DirectButton>
+            </ButtonWrapper>
+          )}
+        </SchoolListHeaderDiv>
+
+        {/* 학교 선택 컴포넌트 */}
+        <SchoolListContentDiv>
+          {/* 정렬 관련 툴바 */}
+          <SchoolListToolbarDiv>
+            <span>정렬</span>
+            <BorderLine />
+            <SortByWrapper>
+              {sortByList.map((sortBy, i) => (
+                <SortButton
+                  key={i}
+                  selected={sortBy.label == curSortBy.label}
+                  onClick={() => {
+                    setCurSortBy(sortBy);
+                  }}
+                >
+                  {sortBy.label}
+                </SortButton>
+              ))}
+            </SortByWrapper>
+          </SchoolListToolbarDiv>
+
+          {/* 학교 목록 조회 컴포넌트 */}
+          <SchoolListContent>
+            <SchoolCardWrapper>
+              <SchoolInfo />
+              <SchoolInfo />
+            </SchoolCardWrapper>
+          </SchoolListContent>
+        </SchoolListContentDiv>
+      </div>
+
+      {/* 하단 페이지네이션 */}
+      <PaginationWrapper>
+        <Pagination totalPage={22} page={page} limit={10} setPage={setPage} />
+      </PaginationWrapper>
+    </ContainerDiv>
+  );
+}
