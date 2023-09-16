457ea6e532dcfdf1e50cee6ea8bd9b550663746d
joojae02 <jake010417@gmail.com>
2023-09-16 17:10:30 +0900

    build: page.tsx 만 페이지로 인식되도록 설정

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-}
-
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true,
   },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 };
