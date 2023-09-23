48b5a19be3827d04da07715526b6a8515e640023
joojae02 <jake010417@gmail.com>
2023-09-23 23:13:12 +0900

    feat: 초록 보더 버튼 구현 / 색상은 외부에서 주입

---
+++ src/components/button/GreanBorderButton/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyledButton } from './style';
+
+type ButtonProps = {
+	backgroundColor: string;
+	text: string;
+	textColor: string;
+	onClick: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+export default function GreenMediumButton({ backgroundColor, text, textColor, onClick }: ButtonProps) {
+	return (
+		<StyledButton type="button" backgroundColor={backgroundColor} textColor={textColor} onClick={onClick}>
+			{text}
+		</StyledButton>
+	);
+}
---
+++ src/components/button/GreanBorderButton/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button<{ backgroundColor?: string; textColor?: string }>`
+	width: 5.625rem;
+	height: 2.1875rem;
+	flex-shrink: 0;
+	border-radius: 5px;
+	border: 1px solid #36bf7f;
+	background: ${(props) => props.backgroundColor || '#36BF7F'};
+	color: ${(props) => props.textColor || '#36BF7F'};
+`;
