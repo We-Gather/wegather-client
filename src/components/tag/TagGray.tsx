0a79ba9dc151feff657f90027e093df2929bbbe3
Huiyeong <53117014+klloo@users.noreply.github.com>
2023-10-15 14:15:11 +0900

    refactor: 동아리 메인 화면 디자인파일에 맞게 수정

--- src/components/tag/TagGray.tsx
+++ src/components/tag/TagGray.tsx
@@ -1,33 +1,32 @@
 import styled from 'styled-components';
 
 const TagWrapper = styled.div`
-	display: inline-flex;
-	margin: 0 0.25rem;
-	padding: 0.3125rem 1.25rem;
-	align-items: flex-start;
-	gap: 0.625rem;
-	border-radius: 50px;
-	background: #E5E5E5;	
+  display: inline-flex;
+  margin: 0 0.25rem;
+  padding: 0.3125rem 1.25rem;
+  align-items: flex-start;
+  gap: 0.625rem;
+  border-radius: 50px;
+  background: #e5e5e5;
 `;
 
-const TagWord = styled.p`
-	color: #585858;
-	font-size: 0.75rem;
-	font-style: normal;
-	font-weight: 400;
-	line-height: normal;
+const TagWord = styled.span`
+  color: #585858;
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
-
 
 interface Props {
-	id: number;
-	text: string;
+  id: number;
+  text: string;
 }
 
-export default function TagOrange ({text} : Props) {
-	return (
-		<TagWrapper>
-			<TagWord>{text}</TagWord>
-		</TagWrapper>
-	);
-};
+export default function TagGray({ text }: Props) {
+  return (
+    <TagWrapper>
+      <TagWord>{text}</TagWord>
+    </TagWrapper>
+  );
+}
--- src/pages/club/main/ClubRecruitCard/index.tsx
+++ src/pages/club/main/ClubRecruitCard/index.tsx
@@ -2,69 +2,77 @@ import { clubRecruitCard } from '@/types/clubRecruitCard';
 import { useState } from 'react';
 import React from 'react';
 import {
-	MainContainer,
-	ClubRecuitContainer,
-	ClubNameDateContainer,
-	DeadLineWrapper,
-	StarButton,
-	EmptyStar,
-	FilledStar,
-	ClubDeadlineCounter,
-	ClubRecruitName,
-	ClubApplicantCount,
-	VerticalLine,
-	ClubDescriptionContainer,
-	ClubDescription,
-	ClubDeadline,
-	ClubSubmitButton,
-	ToggleButton,
-	PreviewWrapper,
+  MainContainer,
+  ClubRecuitContainer,
+  ClubNameDateContainer,
+  DeadLineWrapper,
+  StarButton,
+  EmptyStar,
+  FilledStar,
+  ClubDeadlineCounter,
+  ClubRecruitName,
+  ClubApplicantCount,
+  VerticalLine,
+  ClubDescriptionContainer,
+  ClubDescription,
+  ClubDeadline,
+  ClubSubmitButton,
+  ToggleButton,
+  PreviewWrapper,
 } from './style';
 
 interface Props {
-	recruitInfo: clubRecruitCard;
-} 
+  recruitInfo: clubRecruitCard;
+}
 /*
- * 미리보기 토글 내부는 미구현 
+ * 미리보기 토글 내부는 미구현
  */
 export default function ClubRecruitCard({ recruitInfo }: Props) {
-	const [visible, seVisible] = useState(false);
-	const [isStartClicked, setStarClicked] = useState(false);
+  const [visible, seVisible] = useState(false);
+  const [isStartClicked, setStarClicked] = useState(false);
 
-	const handleClick = () => {
-		setStarClicked(!isStartClicked);
-	};
-	const endDate: Date = new Date(recruitInfo.endDate);
-	const today: Date = new Date();
-	const timeDifference: number = endDate.getTime() - today.getTime();
-	const daysRemaining: number = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  const handleClick = () => {
+    setStarClicked(!isStartClicked);
+  };
+  const endDate: Date = new Date(recruitInfo.endDate);
+  const today: Date = new Date();
+  const timeDifference: number = endDate.getTime() - today.getTime();
+  const daysRemaining: number = Math.ceil(
+    timeDifference / (1000 * 60 * 60 * 24)
+  );
 
-	return (
-		<MainContainer>
-			<ClubRecuitContainer>
-				<ClubNameDateContainer>
-					<DeadLineWrapper>
-						<StarButton onClick={handleClick}>{!isStartClicked ? <EmptyStar /> : <FilledStar />}</StarButton>
-						<ClubDeadlineCounter>Day-{daysRemaining > 0 ? daysRemaining : 0}</ClubDeadlineCounter>
-					</DeadLineWrapper>
-					<ClubRecruitName>{recruitInfo.title}</ClubRecruitName>
-					<ClubApplicantCount>
-						지원자 : <span>{recruitInfo.applicantCount}</span>명
-					</ClubApplicantCount>
-				</ClubNameDateContainer>
-				<VerticalLine />
-				<ClubDescriptionContainer>
-					<ClubDescription>{recruitInfo.description}</ClubDescription>
-					<ClubDeadline>
-						{recruitInfo.startDate} ~ {recruitInfo.endDate}
-					</ClubDeadline>
-				</ClubDescriptionContainer>
-				<ClubSubmitButton>지원하기</ClubSubmitButton>
-			</ClubRecuitContainer>
+  return (
+    <MainContainer>
+      <ClubRecuitContainer>
+        <ClubNameDateContainer>
+          <DeadLineWrapper>
+            <StarButton onClick={handleClick}>
+              {!isStartClicked ? <EmptyStar /> : <FilledStar />}
+            </StarButton>
+            <ClubDeadlineCounter>
+              Day-{daysRemaining > 0 ? daysRemaining : 0}
+            </ClubDeadlineCounter>
+          </DeadLineWrapper>
+          <ClubRecruitName>{recruitInfo.title}</ClubRecruitName>
+          <ClubApplicantCount>
+            지원자 : <span>{recruitInfo.applicantCount}</span>명
+          </ClubApplicantCount>
+        </ClubNameDateContainer>
+        <VerticalLine />
+        <ClubDescriptionContainer>
+          <ClubDescription>{recruitInfo.description}</ClubDescription>
+          <ClubDeadline>
+            {recruitInfo.startDate} ~ {recruitInfo.endDate}
+          </ClubDeadline>
+        </ClubDescriptionContainer>
+        <ClubSubmitButton>지원하기</ClubSubmitButton>
+      </ClubRecuitContainer>
 
-			<PreviewWrapper $visible={visible}>
-				<ToggleButton onClick={() => seVisible(!visible)}>미리보기</ToggleButton>
-			</PreviewWrapper>
-		</MainContainer>
-	);
+      <PreviewWrapper $visible={visible}>
+        <ToggleButton onClick={() => seVisible(!visible)}>
+          미리보기
+        </ToggleButton>
+      </PreviewWrapper>
+    </MainContainer>
+  );
 }
